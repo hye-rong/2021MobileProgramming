f9d2d7c72efd1ddc967f4ba6b44860a7b0b3c8ee
marie043 <sieun043@gmail.com>
2021-06-01 17:22:24 +0900

    김시은-리스트

---
+++ app/src/main/java/com/example/mteamproject/ShowArtListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mteamproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowArtListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_art_list)
+    }
+}
