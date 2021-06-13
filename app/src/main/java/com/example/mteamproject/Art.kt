f66bc4346afe92f70726d771c34bd7027d9efc4b
marie043 <sieun043@gmail.com>
2021-06-13 14:17:47 +0900

    art-김시은

--- app/src/main/java/com/example/mteamproject/Art.kt
+++ app/src/main/java/com/example/mteamproject/Art.kt
@@ -1,5 +1,8 @@
 package com.example.mteamproject
 
 import android.graphics.Bitmap
+import java.util.*
 
-data class Art(val artwork:Bitmap,val title:String, val artist:Artist, val category:String,val price:Int)
+data class Art(var artwork:Bitmap?,var title:String, val artistID:Int, var categorycode:Int,var price:Int,val updatedate:Date?, var soldout:Boolean,val Action:Boolean,val auctionstartdate:Date?,var auctionenddate:Date?){
+    constructor():this(null,"",0,0,0,null,false,false,null,null)
+}
--- app/src/main/java/com/example/mteamproject/Artist.kt
+++ app/src/main/java/com/example/mteamproject/Artist.kt
@@ -2,4 +2,7 @@ package com.example.mteamproject
 
 import android.graphics.Bitmap
 
-data class Artist(val name:String,val icon:Bitmap)
+data class Artist(val id:Int,val name:String,val icon:Bitmap?){
+    constructor():this(0,"",null)
+}
+
